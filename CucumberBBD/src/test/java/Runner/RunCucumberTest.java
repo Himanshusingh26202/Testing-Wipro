34c0b695b86f2bfb78be66f10afd6e5cfcc5c984
Himanshu Kumar Singh <himanshusingh26202@gmail.com>
2026-06-02 16:39:21 +0530

    new

--- CucumberBBD/src/test/java/Runner/RunCucumberTest.java
+++ CucumberBBD/src/test/java/Runner/RunCucumberTest.java
@@ -14,8 +14,8 @@ tags= "@Smoke",
 plugin = {
 "pretty",
 "html:target/htmlreports/report2.html",
-"json:target/jsonreports/report1.jason",
-"junit:target/junitreports/report1.junit"
+"json:target/jsonreports/report1.json",
+"junit:target/junitreports/report1.xml"
 }
 )
 
